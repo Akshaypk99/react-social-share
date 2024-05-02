c96e1a59ce0491a749255d693903c592ff7f1657
Akshay Pk <akshay21pk@gmail.com>
2024-05-02 16:57:19 +0530

    ssr meta tags

--- server/index.js
+++ server/index.js
@@ -3,20 +3,12 @@ const path = require("path");
 const fs = require("fs");
 const axios = require("axios");
 
+const BaseURL = "https://zogglobaltest.com"
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-const BaseURL = "https://zogglobaltest.com"
 
-function replacePlaceholders(data, title, description, metaImage, metaSiteName) {
-  return data
-    .replace(/__TITLE__/g, title)
-    .replace(/__DESCRIPTION__/g, description)
-    .replace(/__META_IMAGE__/g, metaImage)
-    .replace(/__SITE_NAME__/g, metaSiteName);
-}
 
-// Example blog data (replace it with your actual data fetching logic)
 const metaData = {
     title: "Zog Global",
     description: "Zog Global",
@@ -25,11 +17,11 @@ const metaData = {
 };
 
 app.get("*", async (req, res, next) => {
-  // If the request is for a static file, skip processing and serve the file directly
   if (req.url.includes(".")) {
     return next();
   }
 
+  console.log(req.path);
   const filePath = path.resolve(__dirname, "..", "build", "index.html");
   fs.readFile(filePath, "utf8", async (err, data) => {
     if (err) {
@@ -41,28 +33,44 @@ app.get("*", async (req, res, next) => {
     let metaImage = metaData.metaImage;
     let metaSiteName = metaData.metaSiteName;
 
-    // Get the title and description based on the route
+    
     if (req.path.startsWith("/page1/")) {
       const id = req.path.split("/")[2]; // Get the id from the path
-      try {
-        const response = await axios.get(`${BaseURL}/blogs/${id}`);
-        // console.log(response.data);
-        title = response.data.title;
-        metaImage = response.data.image_url;
-        description = response.data.description; // Assuming there's a description
-        
-      } catch (error) {
-        console.error("Error fetching page data:", error);
+      if (id && id !=='undefined') {
+        try {
+          const response = await axios.get(`${BaseURL}/blogs/${id}`);
+          title = response.data.title;
+          metaImage = response.data.image_url;
+          description = response.data.description;
+          
+        } catch (error) {
+          console.error("Error fetching page data:", error.message);
+        }
       }
     }
 
-    console.log(title, description, metaImage, metaSiteName)
-    const modifiedData = replacePlaceholders(data, title, description,metaImage, metaSiteName);
-    res.send(modifiedData);
+    // Append additional meta tags
+    let modifiedData = data.replace("</head>", `
+      <meta name="description" content="${description}"/>
+      <meta property="og:description" content="${description}"/>
+      <meta property="og:title" content="${title}"/>
+      <meta property="og:image" content="${metaImage}" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+      <meta property="og:site_name" content="${metaSiteName}" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="${title}" />
+      <meta name="twitter:description" content="${description}" />
+      <meta name="twitter:image" content="${metaImage}" />
+      <meta name="twitter:image:width" content="1200" />
+      <meta name="twitter:image:height" content="630" />
+    </head>`);
+
+    res.type('text/html').send(modifiedData);
   });
 });
 
-// Serve static files
+
 app.use(express.static(path.resolve(__dirname, "..", "build")));
 
 app.listen(PORT, () => {
